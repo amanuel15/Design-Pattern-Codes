fc2d2e3d6bb4497500e98b4520c58a7e7622cc1e
amanuel15 <32055722+amanuel15@users.noreply.github.com>
2019-11-26 16:29:00 +0300

    Update proxy.java

--- Proxy/proxy/src/proxy.java
+++ Proxy/proxy/src/proxy.java
@@ -1,8 +1,9 @@
 class proxy {
 
     public static void main(String[] args){
-        System.out.println("Main call");
+        
         ExpensiveObjectProxy eop = new ExpensiveObjectProxy();
+        eop.process();
         eop.process();
 
     }
